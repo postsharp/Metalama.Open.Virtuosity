888a8f7f872c445f255550dcbcec80793c76fc83
Jan Hlavac <jan@postsharp.net>
2022-05-06 10:55:25 +0200

    #30313 Updated to the newest Engineering.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -3,19 +3,30 @@
 
 using PostSharp.Engineering.BuildTools;
 using PostSharp.Engineering.BuildTools.Build.Model;
+using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
-using System.Collections.Immutable;
 
-var product = new Product
+var productDependencyDefinition = new DependencyDefinition( "Metalama.Open.Virtuosity", VcsProvider.GitHub, "Metalama" )
 {
-    ProductName = "Metalama.Open.Virtuosity",
-    Solutions = ImmutableArray.Create<Solution>( new DotNetSolution( "Metalama.Open.Virtuosity.sln" ) ),
+    CiBuildTypes = new ConfigurationSpecific<string>(
+        "Metalama_MetalamaOpen_MetalamaOpenVirtuosity_DebugBuild",
+        "Metalama_MetalamaOpen_MetalamaOpenVirtuosity_ReleaseBuild",
+        "Metalama_MetalamaOpen_MetalamaOpenVirtuosity_PublicBuild" ),
+    DeploymentBuildType = "Metalama_MetalamaOpen_MetalamaOpenVirtuosity_PublicDeployment",
+    BumpBuildType = "Metalama_MetalamaOpen_MetalamaOpenVirtuosity_VersionBump"
+};
+
+var product = new Product(productDependencyDefinition)
+{
+    Solutions = new Solution[] { new DotNetSolution( "Metalama.Open.Virtuosity.sln" ) { CanFormatCode = true } },
     PublicArtifacts = Pattern.Create( "Metalama.Open.Virtuosity.sln.nupkg" ),
-    Dependencies = ImmutableArray.Create( Dependencies.PostSharpEngineering, Dependencies.Metalama )
+    Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama },
+    RequiresBranchMerging = true
 };
 
 var commandApp = new CommandApp();
+
 commandApp.AddProductCommands( product );
 
 return commandApp.Run( args );
